58f4ff3870848ffb6ee141cc6a1dd851dc631547
Yun Lin <llmhyy@gmail.com>
2018-03-24 11:56:38 +0800

    minor adjust

--- train.py
+++ train.py
@@ -12,16 +12,21 @@ def add_neuron(inputs, split_dim, activation_function = None):
     :param activation_function:
     :return:
     """
-    x = tf.cast(tf.slice(inputs, [0, split_dim[0]], [tf.shape(inputs)[0], split_dim[1] - split_dim[0]]),tf.float64)
-    Weights = tf.cast(tf.Variable(tf.random_normal([split_dim[1] - split_dim[0], 1])),tf.float64)
+    slice = tf.slice(inputs, [0, split_dim[0]], [tf.shape(inputs)[0], split_dim[1] - split_dim[0]])
+    x = tf.cast(slice, tf.float64)
+
+    W = tf.Variable(tf.random_normal([split_dim[1] - split_dim[0], 1]))
+    Weights = tf.cast(W ,tf.float64)
     # biases = tf.Variable(tf.zeros([1, out_size]) + 0.1)
-    biases = tf.cast(tf.Variable(tf.zeros([1]) + 0.1),tf.float64)
-    Wx_plus_b = tf.matmul(x, Weights) + biases
+    b = tf.Variable(tf.zeros([1]) + 0.1)
+    biases = tf.cast(b,tf.float64)
+
+    Z = tf.matmul(x, Weights) + biases
     # if activation_function is None:
-    #     outputs = Wx_plus_b
+    #     outputs = Z
     # else:
-    #     outputs = activation_function(Wx_plus_b)
-    return Wx_plus_b, activation_function(Wx_plus_b)
+    #     outputs = activation_function(Z)
+    return Z, activation_function(Z)
 
 def normalization(data):
     # add one more layer and return the output of this layer
@@ -57,9 +62,9 @@ def save(sess, saver, checkpoint_dir):
             'NN.model'))
 
 # the 2nd dimension is probably [1, 3]
-split_dims = [[0,1], [0,3], [3,5], [5,6], [6,109], [109,212], [212,315], [315,418], [418,521], [521,624]]
+split_dims = [[0,3], [3,5], [5,6], [6,109], [109,212], [212,315], [315,418], [418,521], [521,624]]
 
-data = np.loadtxt("./data/train.csv",delimiter=',')
+data = np.loadtxt("./data/train.csv", delimiter=',')
 x_data = data[:,1:]
 x_data = normalization(x_data)
 
@@ -70,8 +75,8 @@ xs = tf.placeholder(tf.float64, [None, len(x_data[0])])
 ys = tf.placeholder(tf.float64, [None, 1])
 
 # train
-dprob = 0.5
-learning_rate = 0.001
+dprob = 0.0
+learning_rate = 0.01
 beta1 = 0.5
 
 #build neural network
@@ -79,16 +84,16 @@ for i in range(len(split_dims)):
     if i == 0:
         logit, hidden_layer1 = add_neuron(xs, split_dims[i], activation_function = tf.nn.relu)
     else:
-        templogit, tempprob = add_neuron(xs, split_dims[i], activation_function=tf.nn.relu)
+        templogit, tempprob = add_neuron(xs, split_dims[i], activation_function = tf.nn.relu)
         hidden_layer1 = tf.concat([hidden_layer1, tempprob], 1)
 
 pd_Weights = tf.cast(tf.Variable(tf.random_normal([len(split_dims), 1])), tf.float64)
 pd_biases = tf.cast(tf.Variable(tf.zeros([1]) + 0.1), tf.float64)
-pd_logit = tf.matmul(hidden_layer1, pd_Weights) + pd_biases
-pd_prob = tf.nn.sigmoid(pd_logit)
+pd_Z = tf.matmul(hidden_layer1, pd_Weights) + pd_biases
+pd_prob = tf.nn.sigmoid(pd_Z)
 t_vars = tf.trainable_variables()
 
-loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=pd_logit, labels=ys))
+loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=pd_Z, labels=ys))
 train_step = tf.train.AdamOptimizer(learning_rate, beta1 = beta1).minimize(loss, var_list=t_vars)
 
 #initialization
@@ -123,3 +128,6 @@ for i in range(len(prediction_value)):
         else:
             wrong_1 += 1
 print(right_0, wrong_0, right_1, wrong_1)
+print("accuracy for 0: ", right_0/(right_0+wrong_0))
+print("accuracy for 1: ", right_1/(right_1+wrong_1))
+print("total accuracy: ", (right_1+right_0)/(right_1+wrong_1+right_0+wrong_0))
