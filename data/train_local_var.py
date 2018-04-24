e0489f7f46f236893a4ec122715564037a1daba4
Yun Lin <llmhyy@gmail.com>
2018-04-24 20:24:40 +0800

    minor

--- data/train_local_var.py
+++ data/train_local_var.py
@@ -9,7 +9,7 @@ train_file = "./data_data/local_var.csv"
 test_file = "./data_data/local_var_test.csv"
 
 split_dims = [[0,1], [1,3], [3,5], [5,6], [6,109], [109,212], [212,315], [315,418], [418,521], [521,624]]
-split_dims = [[0,1], [1,3], [3,5], [5,6]]
+# split_dims = [[0,1], [1,3], [3,5], [5,6]]
 
 usecol=[]
 for i in range(4, 628):
@@ -17,6 +17,32 @@ for i in range(4, 628):
 
 x_data = np.loadtxt(train_file, delimiter=',', usecols=usecol)
 y_data = np.loadtxt(train_file, delimiter=',', usecols=[3], ndmin=2)
+
+positive_data = []
+negative_data = []
+for i in range(len(x_data)):
+    d = x_data[i]
+    if y_data[i][0]==1 :
+        positive_data.append(d)
+    else:
+        negative_data.append(d)
+
+enhanced_data = []
+loop = int(len(negative_data)/len(positive_data)) - 1
+for i in range((int)(loop)):
+    for j in range(len(positive_data)):
+        enhanced_data.append(positive_data[j])
+
+x_array = np.asarray(enhanced_data)
+x_data = np.append(x_data, x_array, axis=0)
+
+y_array = np.ones((len(x_array), 1), dtype=float)
+y_data = np.append(y_data, y_array, axis=0)
+
+randomize = np.arange(len(y_data))
+np.random.shuffle(randomize)
+x_data = x_data[randomize]
+y_data = y_data[randomize]
 
 #define placeholder for inputs
 xs = tf.placeholder(tf.float32, [None, len(x_data[0])], name="Input")
@@ -28,7 +54,7 @@ learning_rate = 0.05
 iteration_time = 2000
 beta1 = 0.5
 random_seed = 0;
-cost_threshold = 0.42
+cost_threshold = 0.55
 
 random.seed(random_seed)
 np.random.seed(random_seed)
